7aa4580d9647954ef770056305a10ab3bc83386b
Reza (Sam) Aghamohammadi <re.agha.mo@gmail.com>
2024-05-31 23:49:51 +0330

    20240531

---
+++ utilslogging/__init__.py
@@ -0,0 +1,4 @@
+try:
+    from utilslogging import logo
+except ImportError:
+    from utilslogging.utilslogging import logo
--- utilspreparelogger/__init__.py
+++
@@ -1,4 +0,0 @@
-try:
-    from utilspreparelogger import logo
-except ImportError:
-    from utilspreparelogger.utilspreparelogger import logo
